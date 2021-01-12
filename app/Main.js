9a0ae91da072368283916615afa62cfe6d777711
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2021-01-12 13:08:09 -0500

    Add react components

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
 
-function ExampleComponent() {
+function Main() {
   return (
-    <div>
-      <h1>This is our app!!!</h1>
-      <p>The sky is blue and grass is green</p>
-    </div>
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
   );
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"));
+ReactDOM.render(<Main />, document.querySelector("#app"));
 
 if (module.hot) {
   module.hot.accept();
